2c22f1d564024c90619a81b7d44e568f3b268212
Musisin228 <90610962+Musisin228@users.noreply.github.com>
2021-10-29 23:30:30 +0300

    Dz

---
+++ Labs226-2021/MaxDelitel/MaxDelitel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Rafil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество ячеек массива");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Console.Write("Ваш массив выглядит так:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int a = array[i];
+                for (int j = array[i] - 1; j > 0; j -= 1)
+                {
+                    int b = a % j;
+                    if (b == 0)
+                    {
+                        Console.WriteLine($"{j} является максимальным делителем для {i + 1}-го элемента массива");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
